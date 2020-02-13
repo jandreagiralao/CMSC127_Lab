869a89f88e984a696735e8058ace302bf13748cf
kjeanne13 <kailamillana13@gmail.com>
2020-02-13 10:18:11 +0800

    Update admin.php

--- PE01/admin.php
+++ PE01/admin.php
@@ -1,57 +1,51 @@
 <?php
-session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $_SESSION["username"] = $_REQUEST["username"];
-    $_SESSION["password"] = $_REQUEST["password"];
-}
+    session_start();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $_SESSION["username"] = $_REQUEST["username"];
+        $_SESSION["password"] = $_REQUEST["password"];
+    }
 
-if ($_SESSION["username"] != "admin" || $_SESSION["password"] != "admin") {
-    header("Location: ./main.php");
-}
+    if ($_SESSION["username"] != "admin" || $_SESSION["password"] != "admin") {
+        header("Location: ./main.php");
+    }
 
-unset($_SESSION["username"]);
-unset($_SESSION["password"]);
+    unset($_SESSION["username"]);
+    unset($_SESSION["password"]);
 ?>
 
 <!DOCTYPE html>
 <html>
 
-<head>
-    <link rel="stylesheet" type="text/css" href="./admin.css">
-</head>
+    <head>
+        <link rel="stylesheet" type="text/css" href="./admin.css">
+    </head>
 
-<body>
-    <form action="./viewXdelete.php" method="post">
-        <input name='username' type='hidden' value="admin">
-        <input name='password' type='hidden' value="admin">
-        <input id='delete' type="submit" value="View applicant's page">
-    </form>
-    <!--
-        <select onchange="location = this.value">
-            <option value="./main.php">Home</option>
-            <option value="./viewXdelete.php">Applican'ts page</option>
-        </select>     --->
+    <body>
+        <form action="./viewXdelete.php" method="post">
+            <input name='username' type='hidden' value="admin">
+            <input name='password' type='hidden' value="admin">
+            <input id='delete' type="submit" value="View applicant's page">
+        </form>
 
-    <div class="dropdown">
-        <button>Manage</button>
-        <div class="dropdown-list">
-            <form action="#" method="post">
-                <input name='username' type='hidden' value="admin">
-                <input name='password' type='hidden' value="admin">
-                <input id='' type="submit" value="Education">
-            </form>
-            <form action="#" method="post">
-                <input name='username' type='hidden' value="admin">
-                <input name='password' type='hidden' value="admin">
-                <input id='' type="submit" value="Scholarship">
-            </form>
-            <form action="#" method="post">
-                <input name='username' type='hidden' value="admin">
-                <input name='password' type='hidden' value="admin">
-                <input id='' type="submit" value="Major Field">
-            </form>
+        <div class="dropdown">
+            <button>Manage</button>
+            <div class="dropdown-list">
+                <form action="#" method="post">
+                    <input name='username' type='hidden' value="admin">
+                    <input name='password' type='hidden' value="admin">
+                    <input id='' type="submit" value="Education">
+                </form>
+                <form action="#" method="post">
+                    <input name='username' type='hidden' value="admin">
+                    <input name='password' type='hidden' value="admin">
+                    <input id='' type="submit" value="Scholarship">
+                </form>
+                <form action="#" method="post">
+                    <input name='username' type='hidden' value="admin">
+                    <input name='password' type='hidden' value="admin">
+                    <input id='' type="submit" value="Major Field">
+                </form>
+            </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
