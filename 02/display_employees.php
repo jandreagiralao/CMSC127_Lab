fcf0f6d13d534d45e0e7c612bd0da76f1ade2b8a
kjeanne13 <kailamillana13@gmail.com>
2020-03-10 11:22:01 +0800

    added display_employees.php code

---
+++ 02/display_employees.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>List of Employees</title>
+</head>
+
+<body>
+    <section id="container">
+        <section id="content">
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "classicmodels";
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            /*
+						if (mysqli_connect_error())
+							die("Connection failed: ".mysqli_connect_error());
+						else echo "Connected successfully <br>";
+						*/
+            $sql = "SELECT * from employees ORDER BY lastName ASC";
+            $result = $conn->query($sql);
+            ?>
+
+            <table>
+                <tr>
+                    <th>Employee Number</th>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Extension</th>
+                    <th>Email</th>
+                    <th>Office Code</th>
+                    <th>Reports To</th>
+                    <th>Job Title</th>
+                </tr>
+
+                <?php
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                ?>
+                        <tr>
+                            <td>
+                                <?php echo $row['employeeNumber'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['lastName'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['firstName'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['extension'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['email'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['officeCode'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['reportsTo'] ?>
+                            </td>
+                            <td>
+                                <?php echo $row['jobTitle'] ?>
+                            </td>
+                        </tr>
+                <?php
+                    }
+                } else echo "Database is empty";
+                $conn->close();
+                ?>
+            </table>
+        </section>
+    </section>
+</body>
