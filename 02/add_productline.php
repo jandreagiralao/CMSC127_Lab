60838f7afe8a4019fe0f1650a872678ffc389a11
jfalfeche <jfalfeche27@gmaill.com>
2020-03-11 18:51:27 +0800

    images file upload needs fixing

--- 02/add_productline.php
+++ 02/add_productline.php
@@ -30,50 +30,35 @@
             $textDescription = $_POST['textDescription'];
             $htmlDescription = $_POST['htmlDescription'];
             //Image
-            $target_dir = "uploads/";
-            $target_file = $target_dir . basename($_FILES["image"]["name"]);
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-            // Check if image file is a actual image or fake image
-            if(isset($_POST["submit"])) {
-                $check = getimagesize($_FILES["image"]["tmp_name"]);
-                if($check !== false) {
-                    echo "File is an image - " . $check["mime"] . ".";
-                    $uploadOk = 1;
-                } else {
-                    echo "File is not an image.";
-                    $uploadOk = 0;
-                }
-            }
-            // Check if file already exists
-            if (file_exists($target_file)) {
-                echo "Sorry, file already exists.";
-                $uploadOk = 0;
-            }
-            // Check file size
-            if ($_FILES["image"]["size"] > 500000) {
-                echo "Sorry, your file is too large.";
-                $uploadOk = 0;
-            }
-            // Allow certain file formats
-            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif" ) {
-                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                $uploadOk = 0;
-            }
-            // Check if $uploadOk is set to 0 by an error
-            if ($uploadOk == 0) {
-                echo "Sorry, your file was not uploaded.";
-            }
+              $name = $_FILES['image']['name'];
+              $target_dir = "upload/";
+              $target_file = $target_dir . basename($_FILES["image"]["name"]);
 
-            //insert values to table
-            $sql = "INSERT INTO productlines VALUES ('$productLine', '$textDescription', '$htmlDescription', '$target_file')";
+              // Select file type
+              $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-            if ($conn->query($sql) === TRUE)
-                echo "<h2 style='color: red;'>Data inserted successfuly.</h2>";
-            else
-                echo "<h2 style='color: red;'>Error adding data:" . $conn->error . "</h2>";
-            $conn->close();
+              // Valid file extensions
+              $extensions_arr = array("jpg","jpeg","png","gif");
+
+              // Check extension
+              if( in_array($imageFileType,$extensions_arr) ){
+
+                 // Insert record
+                 $query = "insert into images(name) values('".$name."')";
+                 mysqli_query($conn,$query);
+
+                 // Upload file
+                 $image = move_uploaded_file($_FILES['image']['tmp_name'],$target_dir.$name);
+
+                 //insert values to table
+                 $sql = "INSERT INTO productlines VALUES ('$productLine', '$textDescription', '$htmlDescription', '$image')";
+
+                 if ($conn->query($sql) === TRUE)
+                     echo "<h2 style='color: red;'>Data inserted successfuly.</h2>";
+                 else
+                     echo "<h2 style='color: red;'>Error adding data:" . $conn->error . "</h2>";
+                 $conn->close();
+              }
         }
         ?>
     </div>
