035916221856d06446e2da08d9fd6b443ecc2b85
jandreagiralao <60372091+jandreagiralao@users.noreply.github.com>
2020-03-11 15:17:39 +0800

    Update create_employee.php

--- 02/create_employee.php
+++ 02/create_employee.php
@@ -1,13 +1,30 @@
+<?php 
+
+    $error_msg = "";
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "classicmodels";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    
+    if (mysqli_connect_error())
+        die("Connection failed: ".mysqli_connect_error());
+    else echo "Connected successfully <br>";
+                    
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Orders</title>
+    <title>Add Employee</title>
 </head>
 
 <body>
     <div class="container">
-        <form class="add_form" action="create_employees.php" method="post">
+        <form class="add_form" action="add_employee.php" method="post">
             <p>Employee Number</p>
             <input type="number" name="employeeNumber" required> <br>
             <p>Last Name</p>
@@ -19,9 +36,42 @@
             <p>Email</p>
             <input type="text" name="email" required> <br>
             <p>Office Code</p>
-            <input type="text" name="officeCode" required> <br>
             <p>Reports To</p>
-            <input type="number" name="reportsTo" required> <br>
+            <select name="officeCode" >
+                <option value="null">Select Here</option>
+                <?php
+                    $result = $conn->query("SELECT * FROM offices");
+
+                    if($result->num_rows > 0)
+                    {
+                        while($row = $result->fetch_assoc()) 
+                        { 
+                            ?>
+                        <option value="<?php echo $row['officeCode']; ?>" > <?php echo $row['officeCode'] ?> </option>
+                <?php   
+                        }
+                    }
+                ?>
+            </select>
+
+            <p>Reports To</p>
+            <select name="reportsTo" >
+                <option>Select Here</option>
+                <?php
+                    $result = $conn->query("SELECT * FROM employees");
+
+                    if($result->num_rows > 0)
+                    {
+                        while($row = $result->fetch_assoc()) 
+                        { 
+                            ?>
+                        <option value="<?php echo $row['employeeNumber']; ?>" > <?php echo $row['employeeNumber'] ?> </option>
+                <?php   
+                        }
+                    }
+                ?>
+            </select>
+
             <p>Job Title</p>
             <input type="text" name="jobTitle" required> <br>
             <input type="submit" name="submit" value="Submit">
